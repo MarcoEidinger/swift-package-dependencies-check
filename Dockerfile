89809383b5cacddfc5a283f2e84acc8f30bd67d8
Marco Eidinger <marco.eidinger@sap.com>
2022-01-25 15:04:26 -0800

    v2: release-notes and a reusable workflow

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,19 @@
-# Container image that runs your code
-FROM swift:5.3
+FROM swift:5.5
+
+ARG SWIFT_RELEASENOTES_REPOSITORY="https://github.com/SwiftPackageIndex/ReleaseNotes.git"
+ARG SWIFT_RELEASENOTES_BRANCH="main"
+
 LABEL Description="Docker Container for GitHub action swift-package-dependencies-check"
 LABEL repository="http://github.com/MarcoEidinger/swift-package-dependencies-check/edit/main/Dockerfile"
 LABEL maintainer="Marco Eidinger <eidingermarco@gmail.com>"
 
-# Copies your code file from your action repository to the filesystem path `/` of the container
-COPY entrypoint.sh /entrypoint.sh
+RUN git clone -b  $SWIFT_RELEASENOTES_BRANCH $SWIFT_RELEASENOTES_REPOSITORY _swift-release-notes \
+    && cd _swift-release-notes \
+    && git checkout 2fdf8b0c46fd08dfd70ce8466487b8fa06261b53 \
+    && make install \
+    && cd
 
-# Code file to execute when the docker container starts up (`entrypoint.sh`)
-ENTRYPOINT ["/entrypoint.sh"]
+ADD entrypoint.sh /usr/local/bin/entrypoint
+RUN chmod +x /usr/local/bin/entrypoint
+
+ENTRYPOINT [ "entrypoint" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,34 @@
-#!/bin/sh -l
+#!/usr/bin/env bash
 isMutating=$1
 echo "### Current Package Dependencies (swift package show-dependencies)"
 swift package show-dependencies
-OUTPUT=""
+SPU_RESULT=""
 if [ "$isMutating" = true ] || [ "$isMutating" = 'true' ]
 then
     echo "### Check and Update Packages Dependencies if they are outdated (swift package update)"
-    OUTPUT=$(swift package update)
+
+    echo "#### run swift-release-notes to get details about changes"
+    NOTES_EXTRACT="$(swift-release-notes . | grep -A 100 "changed.")"
+    # Multiline outputs need special treatment
+    # See https://github.community/t5/GitHub-Actions/set-output-Truncates-Multiline-Strings/m-p/38372#M3322
+    NOTES=$(cat <<-EOF
+	$NOTES_EXTRACT
+	EOF
+	)
+    NOTES="${NOTES//'%'/'%25'}"
+    NOTES="${NOTES//$'\n'/'%0A'}"
+    NOTES="${NOTES//$'\r'/'%0D'}"
+    echo "::set-output name=releaseNotes::$NOTES"
+
+    echo "### Run swift package update"
+    SPU_RESULT=$(swift package update)
 else
-	echo "### Check Packages Dependencies if they are up-to-date (swift package update --dry-run)"
-	OUTPUT=$(swift package update --dry-run)
+    echo "### Check Packages Dependencies if they are up-to-date (swift package update --dry-run)"
+    SPU_RESULT=$(swift package update --dry-run)
 fi
-echo "$OUTPUT"
-if echo "$OUTPUT" | grep -q "0 dependencies have changed.\|Everything is already up-to-date"; then
-  exit 0
+echo "$SPU_RESULT"
+if echo "$SPU_RESULT" | grep -q "0 dependencies have changed.\|Everything is already up-to-date"; then
+    exit 0
 else
-  exit 1
+    exit 1
 fi
