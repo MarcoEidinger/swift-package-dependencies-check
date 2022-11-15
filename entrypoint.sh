89f18bab62fffc0b0136c9b67878b145157ee93c
Marco Eidinger <eidingermarco@gmail.com>
2022-11-14 16:46:35 -0800

    format document

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,39 +9,39 @@ echo "### Current Package Dependencies (swift package show-dependencies)"
 swift package show-dependencies
 SPU_RESULT=""
 if [ "$isMutating" = true ] || [ "$isMutating" = 'true' ]; then
-	echo "### Check and Update Packages Dependencies if they are outdated (swift package update)"
+    echo "### Check and Update Packages Dependencies if they are outdated (swift package update)"
 
-	echo "#### run swift-release-notes to get details about changes"
-	NOTES_EXTRACT="$(swift-release-notes . | grep -A 100 "changed.")"
-	# Multiline outputs need special treatment
-	NOTES=$(
-		cat <<-EOF
+    echo "#### run swift-release-notes to get details about changes"
+    NOTES_EXTRACT="$(swift-release-notes . | grep -A 100 "changed.")"
+    # Multiline outputs need special treatment
+    NOTES=$(
+        cat <<-EOF
 			$NOTES_EXTRACT
 		EOF
-	)
+    )
     # https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#multiline-strings
-    echo "releaseNotes<<EOF" >> $GITHUB_OUTPUT
-    echo "$NOTES" >> $GITHUB_OUTPUT
-    echo "EOF" >> $GITHUB_OUTPUT
+    echo "releaseNotes<<EOF" >>$GITHUB_OUTPUT
+    echo "$NOTES" >>$GITHUB_OUTPUT
+    echo "EOF" >>$GITHUB_OUTPUT
 
-	echo "### Run swift package update"
-	SPU_RESULT=$(swift package update 2>&1)
+    echo "### Run swift package update"
+    SPU_RESULT=$(swift package update 2>&1)
 else
-	echo "### Check Packages Dependencies if they are up-to-date (swift package update --dry-run)"
-	SPU_RESULT=$(swift package update --dry-run 2>&1)
+    echo "### Check Packages Dependencies if they are up-to-date (swift package update --dry-run)"
+    SPU_RESULT=$(swift package update --dry-run 2>&1)
 fi
 echo "$SPU_RESULT"
 if echo "$SPU_RESULT" | grep -q "0 dependencies have changed.\|Everything is already up-to-date"; then
-    echo outdatedDependencies=false >> $GITHUB_OUTPUT
-	exit 0
+    echo outdatedDependencies=false >>$GITHUB_OUTPUT
+    exit 0
 elif echo "$SPU_RESULT" | grep "error: package"; then
-    echo outdatedDependencies=false >> $GITHUB_OUTPUT
-	exit 1
+    echo outdatedDependencies=false >>$GITHUB_OUTPUT
+    exit 1
 else
-    echo outdatedDependencies=true >> $GITHUB_OUTPUT
-	if [ "$failWhenOutdated" = true ] || [ "$failWhenOutdated" = 'true' ]; then
-		exit 1
-	else
-		exit 0
-	fi
+    echo outdatedDependencies=true >>$GITHUB_OUTPUT
+    if [ "$failWhenOutdated" = true ] || [ "$failWhenOutdated" = 'true' ]; then
+        exit 1
+    else
+        exit 0
+    fi
 fi
